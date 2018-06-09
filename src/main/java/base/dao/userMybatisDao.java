e5e7485dd3769b0686a11cfc822271fc8258bf66
yuzongjian <1306320627@qq.com>
2018-06-09 16:21:23 +0800

    增加了mybatis的配置

---
+++ src/main/java/base/dao/userMybatisDao.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package base.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author yuzongjian
+ *
+ * This is a project for the power supply bureau.
+ */
+@Repository
+public class userMybatisDao {
+
+}
