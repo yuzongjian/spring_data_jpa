19f914c75655982244013ace0a9622c012009c9a
yuzongjian <1306320627@qq.com>
2018-06-13 23:24:05 +0800

    添加了quatz定时任务处理订单问题

---
+++ src/main/java/base/job/PaymentOrderJob.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package base.job;
+import org.quartz.JobExecutionContext;  
+import org.quartz.JobExecutionException;  
+import org.springframework.context.ApplicationContext;  
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import base.dao.userDao;
+import base.service.userService;  
+/**
+ * @author yuzongjian
+ *
+ * This is a project for the power supply bureau.
+ */
+/** 
+ * 扫描超过2天未付款的订单关闭 
+ */  
+public class PaymentOrderJob extends QuartzJobBean {  
+  
+    @Override  
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {  
+        ApplicationContext applicationContext = (ApplicationContext) context.getJobDetail().getJobDataMap()  
+                .get("applicationContext");  
+        //时间参数，当前时间向前推2天  
+        applicationContext.getBean(userService.class).countByUsername("34");
+    }  
+  
+}  
--- src/main/java/base/service/userService.java
+++ src/main/java/base/service/userService.java
@@ -3,6 +3,8 @@
  */
 package base.service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +44,8 @@ public class userService {
 		 return userDao.count();
 	 }
 	 public long countByUsername(String username) {
+		 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+         System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
 		 return userDao.countByUsername( username);
 	 }
 	 public int update(String username , int id) {
